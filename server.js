9bdc312afbf84d3c61af5d97a64998b563e6185a
Vladimir Galynsky <galynskyy@gmail.com>
2016-12-23 13:14:04 +0000

    add server

---
+++ server.js
@@ -0,0 +1,14 @@
+var static = require("node-static"),
+	port = 8080,
+	http = require("http");
+
+var file = new static.Server("./public", {
+	cache: 3600,
+	gzip: true
+});
+
+http.createServer(function(request, response) {
+	request.addListener("end", function() {
+    	file.serve(request, response);
+    }).resume();
+}).listen(port);
