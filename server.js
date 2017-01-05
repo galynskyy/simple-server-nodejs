32d4951d34db29a6679c6add08d5ea30dd82154a
Vladimir Galynsky <galynskyy@gmail.com>
2017-01-05 10:15:48 +0700

    Update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var static = require("node-static"),
 	port = 8080,
 	http = require("http");
 
-var file = new static.Server("./public", {
+var file = new static.Server("./", {
 	cache: 3600,
 	gzip: true
 });
